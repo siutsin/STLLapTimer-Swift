cf4dfb2efbd502d82a5ef4cecdac0c76b58f0dc9
Simon Li <simon824@gmail.com>
2014-09-03 19:01:27 +0800

    port variables and storyboard

--- LapTimer-Swift/ViewController.swift
+++ LapTimer-Swift/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-                            
-    override func viewDidLoad() {
+class ViewController: UITableViewController
+{
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var filterView: GPUImageView!
+    @IBOutlet weak var titleTimerLabel: UILabel!
+    
+    // MARK: - Variables / Constants
+    
+    let sensitivity = 0.2
+    let cooldownPeriod = 2.0
+    
+    var startTime: NSDate?
+    var timer: NSTimer?
+    var cooldown = false
+    var lapCounter: NSInteger = 0
+    var lapTimeArray = [String]()
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.videoCamera.startCameraCapture()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Motion Detector
+    
+    lazy var videoCamera: GPUImageVideoCamera =
+    {
+        var tempVideoCamera = GPUImageVideoCamera(sessionPreset: (AVCaptureSessionPreset352x288 as String), cameraPosition: AVCaptureDevicePosition.Back)
+        tempVideoCamera.outputImageOrientation = UIApplication.sharedApplication().statusBarOrientation
+        var filter = GPUImageMotionDetector()
+        filter.motionDetectionBlock = { (CGPoint motionCentroid, CGFloat motionIntensity, CMTime frameTime) -> Void in
+            if (motionIntensity > 0.2) {
+                self.lap()
+            }
+        }
+        tempVideoCamera.addTarget(filter)
+        tempVideoCamera.addTarget(self.filterView)
+        return tempVideoCamera
+    }()
+    
+    // MARK: - Lap Timer
+    
+    func lap() {
+        //
     }
-
+    
+    // MARK: - UITableView
+    
+    override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
+        return self.lapTimeArray.count
+    }
+    
+//    override func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath: NSIndexPath?) -> UITableViewCell? {
+//        var cell = tableView?.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell
+//        cell!.textLabel.text = self.lapTimeArray.count > 0 ? self.lapTimeArray[indexPath.row] : ""
+//        return cell
+//    }
+    
+//    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+//        var cell = tableView?.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell
+//        cell!.textLabel.text = self.lapTimeArray.count > 0 ? self.lapTimeArray[indexPath.row] : ""
+//        return cell
+//    }
 
 }
 
