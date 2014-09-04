92832d1280746796a5bf1d08ab96a412b68edd10
Simon Li <simon824@gmail.com>
2014-09-04 14:50:13 +0800

    add playground

--- Playground.playground/section-1.swift
+++ Playground.playground/section-1.swift
@@ -3,3 +3,8 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let ti: Double = Double(2.3477777777)
+let x: Double = fmod(ti, 1) * 100
+let y: Double = round(x)
+let centiseconds: Int = Int(round(fmod(ti, 1) * 100))
